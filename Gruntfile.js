c6d61551a3387f530fc64e5459236b88f7acc727
Dennis Boldt <boldt@itm.uni-luebeck.de>
2014-12-01 20:29:22 +0100

    JSlint works in Brackets and with grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    "use strict";
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: grunt.file.readJSON('jshint.json'),
-        jslint: grunt.file.readJSON('jslint.json'),
+        jslint: grunt.file.readJSON('jslint.json')
     });
 
     grunt.loadNpmTasks('grunt-jslint');
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
-//    grunt.registerTask('check', ['jshint', 'jslint']); 
-//    grunt.registerTask('default', ['jshint', 'jslint']); 
-
+    //    grunt.registerTask('check', ['jshint', 'jslint']); 
+    grunt.registerTask('default', ['jslint']);
 };
---
+++ README.md
@@ -0,0 +1,22 @@
+# Setup
+
+Run `npm install` to install the dependencies from the `package.json`.
+
+# Jslint configuration
+
+* `.brackets.json` configures JSLint for Brackets
+* `jslint.json` configures JSLint for `grunt jslint`.
+
+Both json-files should be same configured.
+
+
+# Use Brackets
+
+Import project in Brackets and open `Gruntfile.js` and `js/test.js`; no errors are shown.
+
+# Use grunt
+
+Call `grunt jslint` in the root of the project.
+
+* Brackets and `grunt jslint` are returning the same result
+
