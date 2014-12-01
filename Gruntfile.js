de9d9363048966ab40b0ca947382eb3ffbc43f9c
Dennis Boldt <boldt@itm.uni-luebeck.de>
2014-12-01 18:55:04 +0100

    Added Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: grunt.file.readJSON('jshint.json'),
+        jslint: grunt.file.readJSON('jslint.json'),
+    });
+
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+//    grunt.registerTask('check', ['jshint', 'jslint']); 
+//    grunt.registerTask('default', ['jshint', 'jslint']); 
+
+};
