889ce01e4326db7c075f98d06f6e613c55bc9b5c
Dennis Boldt <boldt@itm.uni-luebeck.de>
2014-12-01 17:47:27 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,16 @@
+/*global console */
+
+(function () {
+    
+    "use strict";
+
+    function test() {
+        var i = 0;
+        for (i = 0; i <= 10; i++) {
+            console.log(i);
+        }
+    }
+
+    test();
+    
+}());
